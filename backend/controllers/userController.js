4860b3488b71752af7ffb955bb4870ea3c0dba22
Adam Kwoka <adam.kwoka99@gmail.com>
2023-10-16 06:13:09 +0200

    initialize auth process implementation

---
+++ backend/controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require('../models/userModel');
+
+const loginUser = async (req, res) => {
+  res.json({ message: 'login user'} );
+}
+
+const signupUser = async (req, res) => {
+  res.json({ message: 'signup user'} );
+}
+
+module.exports = {
+  loginUser,
+  signupUser
+}
