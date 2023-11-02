4c73388fe8668122ca3fd01b5a47ddf76b9d2073
Adam Kwoka <adam.kwoka99@gmail.com>
2023-11-02 06:40:23 +0100

    chore: add auth context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { WorkoutContextProvider } from './context/WorkoutContext';
+import { AuthContextProvider } from './context/AuthContext';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WorkoutContextProvider>
-      <App />
-    </WorkoutContextProvider>
+    <AuthContextProvider>
+      <WorkoutContextProvider>
+        <App />
+      </WorkoutContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
