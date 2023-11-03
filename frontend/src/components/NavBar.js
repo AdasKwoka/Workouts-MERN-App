fc058d09d22004c33fcca5dcfbde6ffc3de14db8
Adam Kwoka <adam.kwoka99@gmail.com>
2023-11-03 06:00:07 +0100

    chore: add useLogout hook

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
 
 const NavBar = () => {
+  const { logout } = useLogout();
+
+  const handleLogout = () => logout();
+
   return (
     <header>
       <div className="container">
@@ -9,6 +14,9 @@ const NavBar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
+          <div>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
           <div>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
